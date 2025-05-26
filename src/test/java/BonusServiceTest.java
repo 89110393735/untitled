19e676cddcc255700bbb7ae471a13d6370c3011f
Denis <deniskazarinov@gmail.com>
2025-05-26 03:05:46 +0300

    initial commit

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,56 @@
+import org.testng.annotations.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class BonusServiceTest {
+
+    @Test
+    public void testshouldCalculateForRegisteredAndUnderLimit () {
+        BonusService service = new BonusService();
+
+        long amount = 1_000;
+        boolean registered = true;
+        long expected = 30;
+        long actual = service.calculate(amount, registered);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testshouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testshouldCalculateForRegister() {
+        BonusService service = new BonusService();
+
+        long amount = 5_000;
+        boolean registered = false;
+        long expected = 50;
+
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testshouldCalculateForRegisteredAndAnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1_000_000;
+        boolean registered = false;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
